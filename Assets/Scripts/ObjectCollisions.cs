692f3420854e53a76eed77b794cc2ade34e47818
Tatiana Ian <ian.tatiana.v@gmail.com>
2021-10-11 07:55:20 +0300

    Init commit

---
+++ Assets/Scripts/ObjectCollisions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
+public class ObjectCollisions : MonoBehaviour
+{
+    public enum ObjectType
+    {
+        TStone, TFire, TBricks, TTree, TFlower
+    };
+
+    [SerializeField]
+    public ObjectType type;
+
+    public GameObject MixedObject;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ObjectCollisions obj = gameObject.GetComponent<ObjectCollisions>();
+        if (obj.type == type) return;
+
+        if (obj.type == ObjectType.TStone && type == ObjectType.TFire)
+        {
+            other.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            Instantiate(MixedObject, gameObject.transform);
+        }
+    }
+}
